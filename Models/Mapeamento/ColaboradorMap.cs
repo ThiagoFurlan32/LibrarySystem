2dc61dd329df6cc5e53e1e05be2582dec9f9c3d0
ThiagoFurlan32 <thiagoferraz10@hotmail.com>
2021-09-12 11:29:03 -0300

    Criações de Mapeamento

---
+++ Models/Mapeamento/ColaboradorMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Models.Mapeamento
+{
+    public class ColaboradorMap
+    {
+    }
+}
