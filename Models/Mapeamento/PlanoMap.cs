85b2c4da82a1937ddf4ad8fe61d9f2352263b192
ThiagoFurlan32 <thiagoferraz10@hotmail.com>
2021-09-12 11:44:23 -0300

    Começo de mapeamento

--- Models/Mapeamento/PlanoMap.cs
+++ Models/Mapeamento/PlanoMap.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using LibrarySystem.Models.Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibrarySystem.Models.Mapeamento
 {
-    public class PlanoMap
+    public class PlanoMap : IEntityTypeConfiguration<Plano>
     {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Plano> builder)
+        {
+            builder.HasKey(p => p.id);
+            builder.Property(p => p.id).ValueGeneratedOnAdd();
+            builder.Property(p => p.nome).HasMaxLength(35).IsRequired();
+            builder.Property(p => p.nome).HasMaxLength(3).IsRequired();
+
+
+
+            builder.ToTable("Planos");
+        }
     }
 }
