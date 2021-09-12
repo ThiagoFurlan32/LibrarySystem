4b14c7f1c071f25b0db35656140133c911a2218f
XDouglasX7 <83845482+XDouglasX7@users.noreply.github.com>
2021-09-12 12:55:06 -0300

    Criada os mapeamentos

--- Models/Mapeamento/MovimentacaoMap.cs
+++ Models/Mapeamento/MovimentacaoMap.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using LibrarySystem.Models.Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LibrarySystem.Models.Mapeamento
 {
-    public class MovimentacaoMap
+    public class MovimentacaoMap : IEntityTypeConfiguration<Movimentacao>
     {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Movimentacao> builder)
+        {
+            builder.HasKey(p => p.id);
+            builder.Property(p => p.id).ValueGeneratedOnAdd();
+            builder.Property(p => p.cliente).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.livro).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.quantidade).HasMaxLength(15).IsRequired();
+            builder.Property(p => p.movimento).HasMaxLength(35).IsRequired();
+        }
     }
 }
